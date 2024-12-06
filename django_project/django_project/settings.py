ba532425f9cf12757c0a8ee8a49617d3bf3f0623
Cosme Enrique Perez Martinez <123501583+Cosmep@users.noreply.github.com>
2024-12-06 10:47:21 -0400

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6gl)x4t1!!pxs()2p+p8-(*&g1!c(xz4=#z1u0q94qx%_9jcw3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.174.9']
+ALLOWED_HOSTS = []
 
 
 # Application definition
